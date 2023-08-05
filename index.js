dbfbba4c322f20967bfb95cae9e0b1b472d6d483
joaoLazari <eng.jplazzari@gmail.com>
2023-08-04 21:09:30 -0300

    Second commit

--- index.js
+++ index.js
@@ -12,10 +12,64 @@ document.addEventListener("DOMContentLoaded", async () =>{
     data.forEach(hotel => {
         const div = document.createElement('div');
         div.className = "card";
-        div.innerText = `${hotel.name} ${hotel.country}`;
-        var photo = hotel.photo;
+        div.id = `${hotel.name}`;
+        const photo = hotel.photo;
         div.style.backgroundImage = `url(${photo})`;
-
         document.querySelector("main").appendChild(div)
+
+       
+        const h2 = document.createElement('h2');
+        h2.innerText = `${hotel.name}`;
+        h2.className = "hotelName";
+        document.getElementById(`${hotel.name}`).appendChild(h2);
+
+        if(hotel.country === "Argentina"){
+            const img = new Image(20, 20);
+            img.src= "./imagens/argentina.png";
+            const country = document.createElement('p');
+            country.className = "country";
+            country.innerHTML = `${img.outerHTML}   ${hotel.country}`;
+            document.getElementById(`${hotel.name}`).appendChild(country);
+        }else if(hotel.country === "Brasil"){
+            const img = new Image(20, 20);
+            img.src= "./imagens/brasil.png";
+            const country = document.createElement('p');
+            country.className = "country";
+            country.innerHTML = `${img.outerHTML}   ${hotel.country}`;
+            document.getElementById(`${hotel.name}`).appendChild(country);
+        }else if(hotel.country === "Chile"){
+            const img = new Image(20, 20);
+            img.src= "./imagens/chile.png";
+            const country = document.createElement('p');
+            country.className = "country";
+            country.innerHTML = `${img.outerHTML}   ${hotel.country}`;
+            document.getElementById(`${hotel.name}`).appendChild(country);
+        }else if(hotel.country === "Uruguay"){
+            const img = new Image(20, 20);
+            img.src= "./imagens/uruguay.png";
+            const country = document.createElement('p');
+            country.className = "country";
+            country.innerHTML = `${img.outerHTML}   ${hotel.country}`;
+            document.getElementById(`${hotel.name}`).appendChild(country);
+        }
+        
+        const rooms = document.createElement('p');
+        rooms.className = "rooms";
+        document.getElementById(`${hotel.name}`).appendChild(rooms);
+        var price = hotel.price;
+        if(price == 1) {
+            rooms.innerText = `${hotel.rooms} rooms - $`
+        }else if (price == 2) {
+            rooms.innerText = `${hotel.rooms} rooms - $$`
+        }else if(price == 3) {
+            rooms.innerText = `${hotel.rooms} rooms - $$$`
+        }else if(price == 4) {
+            rooms.innerText = `${hotel.rooms} rooms - $$$$`
+        }
+
+        const inner_card1 = document.createElement('div');
+        inner_card1.className = "inner-card1";
+        document.getElementById(`${hotel.name}`).appendChild(inner_card1);
+        
     });
 });
